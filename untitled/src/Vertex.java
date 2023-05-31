e3658ff2cc3b2f25f127f8808c07132078f90118
freezge <freezge@gmail.com>
2023-05-31 23:12:28 +0600

    Vertex constructor

--- untitled/src/Vertex.java
+++ untitled/src/Vertex.java
@@ -1,2 +1,14 @@
-public class Vertex {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+    public V getData() {
+        return data;
+    }
 }
