ae8856edd5b47cb7d99da17208c8ea11a14b2e4b
freezge <freezge@gmail.com>
2023-06-01 02:33:42 +0600

    dijkstra comms

--- untitled/src/DijkstraSearch.java
+++ untitled/src/DijkstraSearch.java
@@ -13,37 +13,37 @@ public class DijkstraSearch<V> implements Search<V> {
         Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
         for (Vertex<V> vertex : graph.getVertices()) {
             if (vertex.equals(source)) {
-                vertex.setDistance(0.0);
+                vertex.setDistance(0.0);//change first vertexes distance to 0
             } else {
-                vertex.setDistance(Double.POSITIVE_INFINITY);
+                vertex.setDistance(Double.POSITIVE_INFINITY);//others to infinity
             }
-            distances.put(vertex, vertex.getDistance());
-            queue.add(vertex);
+            distances.put(vertex, vertex.getDistance());//for storing
+            queue.add(vertex);//add the vertices
         }
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()) {//get the vertex with the smallest distance
             Vertex<V> current = queue.poll();
-            if (current.equals(destination)) {
+            if (current.equals(destination)) {//If vertex is found creating path and returning it
                 return createPath(parentMap, destination);
             }
             for (WeightedGraph.WeightedEdge<V> edge : graph.getEdges(current)) {
-                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();
+                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();//neighbor vertex
                 double newDistance = current.getDistance() + edge.getWeight();
                 if (newDistance < neighbor.getDistance()) {queue.remove(neighbor);
-                    neighbor.setDistance(newDistance);
-                    distances.put(neighbor, newDistance);
+                    neighbor.setDistance(newDistance); //changing distance of neighbor
+                    distances.put(neighbor, newDistance);//change distance in storage
                     parentMap.put(neighbor, current);
-                    queue.add(neighbor);
+                    queue.add(neighbor);//add neighbor to q
                 }
             }
         }
-        return Collections.emptyList();
+        return Collections.emptyList();//return an empty list if path is not found
     }
     private List<V> createPath(Map<Vertex<V>,Vertex<V>> map, Vertex<V> destination) {
         List<V> path = new LinkedList<>();
         Vertex<V> current = destination;
         while (current != null) {
-            path.add(0, current.getData());
-            current = map.get(current);
+            path.add(0, current.getData());//add data of vertex to the path
+            current = map.get(current);//moveto parent
         }
         return path;
     }
