312a97b37277aa274177774f7db08392062b64a4
freezge <freezge@gmail.com>
2023-06-01 01:50:32 +0600

    dijkstra starting point

--- untitled/src/DijkstraSearch.java
+++ untitled/src/DijkstraSearch.java
@@ -1,2 +1,16 @@
-public class DijkstraSearch {
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DijkstraSearch<V> implements Search<V> {
+    private WeightedGraph<V> graph;
+
+    public DijkstraSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
+    @Override
+    public List<V> path(Vertex<V> source, Vertex<V> destination) {
+        return null;
+    }
 }
