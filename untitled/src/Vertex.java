9c08a60357f57847ee80ed5d70c11ce83755d191
freezge <freezge@gmail.com>
2023-06-01 02:23:50 +0600

    vertex comms

--- untitled/src/Vertex.java
+++ untitled/src/Vertex.java
@@ -5,22 +5,22 @@ public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
     private double distance = Double.POSITIVE_INFINITY;
-    public Vertex(V data) {
+    public Vertex(V data) {//constructor
         this.data = data;
         this.adjacentVertices = new HashMap<>();
     }
-    public V getData() {
+    public V getData() {//getter
         return data;
     }
 
-    public double getDistance() {
+    public double getDistance() {//getter
         return distance;
     }
-    public void setDistance(double distance){
+    public void setDistance(double distance){//setter
         this.distance = distance;
     }
 
-    public Map<Vertex<V>, Double> getAdjacentVertices(){
+    public Map<Vertex<V>, Double> getAdjacentVertices(){//getter
         return adjacentVertices;
     }
     public void addAdjacentVertex(Vertex<V> destination, double weight) {
