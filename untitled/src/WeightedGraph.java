f0de768dcb2d7a0789a2eae37798eb3ad47ab086
freezge <freezge@gmail.com>
2023-06-01 00:10:45 +0600

    WeightedEdge

--- untitled/src/WeightedGraph.java
+++ untitled/src/WeightedGraph.java
@@ -1,2 +1,22 @@
-public class WeightedGraph {
+public class WeightedGraph<V>{
+    public class WeightedEdge<V>{
+        private Vertex<V> source;
+        private Vertex<V> dest;
+        private double weight;
+        public WeightedEdge(Vertex<V> source, Vertex<V> dest, double weight) {
+            this.source = source;
+            this.dest = dest;
+            this.weight = weight;
+        }
+        public Vertex<V> getSource(){
+            return source;
+        }
+        public Vertex<V> getDest(){
+            return dest;
+        }
+
+        public double getWeight(){
+            return weight;
+        }
+    }
 }
