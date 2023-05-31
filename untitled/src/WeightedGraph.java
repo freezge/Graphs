3cefd7edf7cb7fffd46677f223fc23dfed877900
freezge <freezge@gmail.com>
2023-06-01 00:23:17 +0600

    addVertex method

--- untitled/src/WeightedGraph.java
+++ untitled/src/WeightedGraph.java
@@ -1,22 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WeightedGraph<V>{
-    public class WeightedEdge<V>{
+    public static class WeightedEdge<V>{
         private Vertex<V> source;
-        private Vertex<V> dest;
+        private Vertex<V> destination;
         private double weight;
-        public WeightedEdge(Vertex<V> source, Vertex<V> dest, double weight) {
+        public WeightedEdge(Vertex<V> source, Vertex<V> destination, double weight) {
             this.source = source;
-            this.dest = dest;
+            this.destination = destination;
             this.weight = weight;
         }
         public Vertex<V> getSource(){
             return source;
         }
-        public Vertex<V> getDest(){
-            return dest;
+        public Vertex<V> getDestination(){
+            return destination;
         }
-
         public double getWeight(){
             return weight;
         }
+    }
+    private Map<Vertex<V>, List<WeightedEdge<V>>> map;
+    public WeightedGraph() {
+        this.map = new HashMap<>();
+    }
+    public void addVertex(Vertex<V> vertex) {
+        map.put(vertex, new ArrayList<>());
     }
 }
