21872583030907f19d173084923e6ed3354e336f
freezge <freezge@gmail.com>
2023-06-01 02:00:19 +0600

    vertex changes

--- untitled/src/Vertex.java
+++ untitled/src/Vertex.java
@@ -4,6 +4,7 @@ import java.util.Map;
 public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
+    private double distance = Double.POSITIVE_INFINITY;
     public Vertex(V data) {
         this.data = data;
         this.adjacentVertices = new HashMap<>();
@@ -11,6 +12,14 @@ public class Vertex<V> {
     public V getData() {
         return data;
     }
+
+    public double getDistance() {
+        return distance;
+    }
+    public void setDistance(double distance){
+        this.distance = distance;
+    }
+
     public Map<Vertex<V>, Double> getAdjacentVertices(){
         return adjacentVertices;
     }
